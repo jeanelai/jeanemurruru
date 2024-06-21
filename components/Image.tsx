009b9e631c3ecd493058a3ea35cd848e99267d02
Marat Chen <maratyz@amazon.com>
2024-06-21 19:39:23 +1000

    fix image eslint

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`/ycmurruru${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/ycmurruru${src}`} {...rest} />
 
 export default Image
